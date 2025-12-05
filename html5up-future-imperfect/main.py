6f72c77ae7be6cd8e83f2bc9b8896292afa7ba23
Ian <ianwinwinwin@gmail.com>
2025-12-05 13:10:01 +0800

    做好文章分類了(作者跟種類)

--- html5up-future-imperfect/main.py
+++ html5up-future-imperfect/main.py
@@ -4,6 +4,7 @@ import sqlite3
 app = Flask(__name__)
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DB_PATH = os.path.join(BASE_DIR, "data.db")
+
 def get_articles_by_category(category_name):
     conn = sqlite3.connect(DB_PATH)
     cursor = conn.cursor()
@@ -13,14 +14,40 @@ def get_articles_by_category(category_name):
     conn.close()
     return articles
 
+def get_all_articles():
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+    query = 'SELECT title, content, author, time, category FROM data ORDER BY time DESC'
+    cursor.execute(query)
+    articles = cursor.fetchall()
+    conn.close()
+    return articles
+
+def get_articles_by_author(author_name):
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+    query = 'SELECT title, content, author, time, category FROM data WHERE author = ? ORDER BY time DESC'
+    cursor.execute(query, (author_name,))
+    articles = cursor.fetchall()
+    conn.close()
+    return articles
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    articles = get_all_articles()
+    return render_template('index.html',articles=articles)
+
 @app.route('/category/<string:category_name>')
 def category_posts(category_name):
     articles = get_articles_by_category(category_name)
     return render_template('single.html', category=category_name, articles=articles)
+
+@app.route('/author/<string:author_name>')
+def author_posts(author_name):
+    articles = get_articles_by_author(author_name)
+    return render_template('single.html', author=author_name, articles=articles)
+
+
 
 
 if __name__ == '__main__':
